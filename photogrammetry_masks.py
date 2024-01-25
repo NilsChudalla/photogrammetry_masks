36ad5b71a7428cc211783ac0f2721afa38bf8117
NilsChudalla <62913082+NilsChudalla@users.noreply.github.com>
2024-01-25 16:23:54 +0100

    Initial Commit

---
+++ photogrammetry_masks.py
@@ -0,0 +1,163 @@
+import cv2
+import numpy as np
+
+class color_mask:
+    def __init__(self, name) -> None:
+        
+        color_mask.name = name
+
+
+
+
+    def define_hsv_limits(self, test_im_path):
+        """
+        This function opens a open cv window with sliders. These sliders allow the user to isoalte
+        a color they want to mask
+        """
+        cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+
+        def nothing(x):
+            pass
+
+        # Load image
+        image = cv2.imread(test_im_path)
+
+        # Create a window
+
+
+        # Create trackbars for color change
+        # Hue is from 0-179 for Opencv
+        cv2.createTrackbar('HMin', 'image', 0, 179, nothing)
+        cv2.createTrackbar('SMin', 'image', 0, 255, nothing)
+        cv2.createTrackbar('VMin', 'image', 0, 255, nothing)
+        cv2.createTrackbar('HMax', 'image', 0, 179, nothing)
+        cv2.createTrackbar('SMax', 'image', 0, 255, nothing)
+        cv2.createTrackbar('VMax', 'image', 0, 255, nothing)
+
+        # Set default value for Max HSV trackbars
+        cv2.setTrackbarPos('HMax', 'image', 179)
+        cv2.setTrackbarPos('SMax', 'image', 255)
+        cv2.setTrackbarPos('VMax', 'image', 255)
+
+        # Initialize HSV min/max values
+        hMin = sMin = vMin = hMax = sMax = vMax = 0
+        phMin = psMin = pvMin = phMax = psMax = pvMax = 0
+
+        while(1):
+            # Get current positions of all trackbars
+            hMin = cv2.getTrackbarPos('HMin', 'image')
+            sMin = cv2.getTrackbarPos('SMin', 'image')
+            vMin = cv2.getTrackbarPos('VMin', 'image')
+            hMax = cv2.getTrackbarPos('HMax', 'image')
+            sMax = cv2.getTrackbarPos('SMax', 'image')
+            vMax = cv2.getTrackbarPos('VMax', 'image')
+
+            # Set minimum and maximum HSV values to display
+            lower = np.array([hMin, sMin, vMin])
+            upper = np.array([hMax, sMax, vMax])
+
+            # Convert to HSV format and color threshold
+            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+            mask = cv2.inRange(hsv, lower, upper)
+            result = cv2.bitwise_and(image, image, mask=mask)
+
+            threshhold_dict = {}
+
+            threshhold_dict['hMin'] = hMin
+            threshhold_dict['sMin'] = sMin
+            threshhold_dict['vMin'] = vMin
+            threshhold_dict['hMax'] = hMax
+            threshhold_dict['sMax'] = sMax
+            threshhold_dict['vMax'] = vMax
+
+            # Display result image
+            cv2.imshow('image', result)
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+        cv2.destroyAllWindows()
+
+        self.threshholds = threshhold_dict
+
+    def define_mask(self, image_input, kernel=np.ones((5,5))):
+        """
+        """
+
+        threshhold_dict = self.threshholds
+        # show result
+        cv2.namedWindow('result', cv2.WINDOW_NORMAL)
+
+        def nothing(x):
+            pass
+        cv2.createTrackbar('Iterations', 'result', 0, 10, nothing)
+
+
+        image = cv2.imread(image_input)
+
+        # convert image to hsv
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        # apply mask
+        lower_g = np.array([threshhold_dict['hMin'] ,threshhold_dict['sMin'], threshhold_dict['vMin']])
+        upper_g = np.array([threshhold_dict['hMax'] ,threshhold_dict['sMax'], threshhold_dict['vMax']])
+        mask = cv2.inRange(hsv, lower_g, upper_g)
+
+        # apply dilation to current mask
+        kernel = np.ones((5, 5), np.uint8)
+        while(1):
+            iterations = cv2.getTrackbarPos('Iterations', 'result')
+            dilated_mask = cv2.dilate(mask, kernel, iterations=iterations)
+
+            # apply mask for result verification
+            result = cv2.bitwise_and(image, image, mask=~dilated_mask)
+
+            cv2.imshow('result', result)
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                    break
+                
+        cv2.destroyAllWindows()
+
+        self.dilation_iterations = iterations
+        self.dilation_kernel = kernel
+
+        return dilated_mask
+
+def apply_combined_masks(masks, image_input, output_dir=False):
+    """
+    """
+    
+
+
+    image = cv2.imread(image_input)
+
+    # convert image to hsv
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    
+    mask_list = []
+    
+    for imask in masks:
+        threshhold_dict = imask.threshholds
+        iterations = imask.dilation_iterations
+        kernel = imask.dilation_kernel
+
+        # apply mask
+        lower_g = np.array([threshhold_dict['hMin'] ,threshhold_dict['sMin'], threshhold_dict['vMin']])
+        upper_g = np.array([threshhold_dict['hMax'] ,threshhold_dict['sMax'], threshhold_dict['vMax']])
+        mask = cv2.inRange(hsv, lower_g, upper_g)
+        dilated_mask = cv2.dilate(mask, kernel, iterations=iterations).astype('bool')
+
+        mask_list.append(dilated_mask)
+    
+    combi_mask = np.zeros_like(dilated_mask, dtype=bool)
+
+    for imask in mask_list:
+        combi_mask += imask
+
+    combi_mask = ~combi_mask.astype(bool)
+
+
+    if output_dir != False:
+        input_dir_names = image_input.split("/")
+        file_name = input_dir_names[-1]
+        cv2.imwrite(output_dir + '/' + file_name, combi_mask.astype('uint8')*255) 
+        print('File %s written' %file_name)
